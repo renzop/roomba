c9aca8f6ef4ebb23fcad562973bc9531019d6376
Lorenz Padberg <lorenz.padberg@gmail.com>
2017-11-12 23:09:22 +0100

    Added tests and requirements txt

---
+++ test/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+#
+# Iterativ GmbH
+# http://www.iterativ.ch/
+#
+# Copyright (c) 2015 Iterativ GmbH. All rights reserved.
+#
+# Created on 2017-11-12
+# @author: lorenz.padberg@iterativ.ch
